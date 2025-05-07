d13cdbffd27f06dd14228e054b32714c9b125df7
sanso <sanso@LAPTOP-5518V52B>
2025-05-07 21:49:27 +0530

    Constructor

---
+++ TNS/src/com/tnsif/ConstructorDemo/Book.java
@@ -0,0 +1,21 @@
+package com.tnsif.ConstructorDemo;
+
+public class Book {
+	String title;
+	Book(String title){
+		this.title=title;
+	}
+	Book(Book b){
+		this.title=b.title;
+	}
+	void display() {
+		System.out.println("the title of book "+title);
+	}
+	public static void main(String[] args) {
+		Book ob=new Book("JFS");
+		Book o=new Book(ob);
+		ob.display();
+		o.display();
+	}
+
+}
