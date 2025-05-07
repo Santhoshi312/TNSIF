0f74a7bcb3e1fbe1c6363bfca7be44a0259cb3b8
sanso <sanso@LAPTOP-5518V52B>
2025-05-07 22:02:45 +0530

    String

---
+++ TNS/src/com/tnsif/StringsDemo/StringDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.StringsDemo;
+
+public class StringDemo {
+	public static void main(String args[]) {
+		StringBuffer S1=new StringBuffer();
+		System.out.println(S1.capacity());
+		StringBuffer S2=new StringBuffer("TNSIF");
+		System.out.println(S2.capacity());
+	}
+}
